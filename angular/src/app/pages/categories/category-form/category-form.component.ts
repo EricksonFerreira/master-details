e39d1098741f3170e4080c3ed68a7275cfd19f3c
ericksonferreira <erickson.rinho@hotmail.com>
2020-03-11 01:43:57 -0300

    melhorando CategoryFormComponent

--- angular/src/app/pages/categories/category-form/category-form.component.ts
+++ angular/src/app/pages/categories/category-form/category-form.component.ts
@@ -41,6 +41,15 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked  {
     this.setPageTitle();
   }
 
+  submitForm(){
+    this.submittingForm = true;
+    if (this.currentAction == "new"){
+      this.createCategory();
+    }else{
+      this.updateCategory();
+    }
+  }
+
   // Métodos privados
 
   //Verifica se é a pagina de editar ou de nova categoria
@@ -84,6 +93,47 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked  {
     }else{
       const categoryName = this.category.name || "";
       this.pageTitle = 'Editando Categoria: ' + categoryName;
+    }
+  }
+
+  private createCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+    this.categoryService.create(category)
+    .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private updateCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+    this.categoryService.update(category)
+    .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+
+  }
+
+  private actionsForSuccess(category: Category){
+    toastr.success("Solicitação processada com sucesso!");
+
+    // redireciona e recarrega a pagina do component
+    this.router.navigateByUrl("categories", {skipLocationChange:true}).then(
+      () => this.router.navigate(['categories', category.id, 'edit'])
+    )
+  }
+
+  private actionsForError(error){
+    toastr.error('Ocoorreu um erro ao processar a sua solicitação!');
+
+    this.submittingForm = false;
+
+    if( error.status == 422 ) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+      // Vai retornar neste estilo: ['Nome já existe', 'Descrição está invalida']
+    }else{
+      this.serverErrorMessages = ["Falha na comunicação com o sevidor. Por favor, tente mais tarde."]
     }
   }
 }
