4b703be07d7fd8db0a521473af381994f66ab45a
ericksonferreira <erickson.rinho@hotmail.com>
2020-03-19 20:47:55 -0300

    melhorando a associação entre Entry e Category

--- angular/src/app/pages/entries/shared/entry.service.ts
+++ angular/src/app/pages/entries/shared/entry.service.ts
@@ -4,6 +4,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError, flatMap } from 'rxjs/operators';
 
+import { CategoryService } from '../../categories/shared/category.service';
+
 import { Entry } from './entry.model';
 
 @Injectable({
@@ -12,7 +14,7 @@ import { Entry } from './entry.model';
 export class EntryService {
 
   private apiPath: string = "api/entries";
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient, private categoryService: CategoryService ) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
@@ -30,19 +32,32 @@ export class EntryService {
   }
   
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)    
-      )
-    }
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)    
+          )
+      })
+    )
+  }
   
   update(entry: Entry): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
-    return this.http.put('sadfsadf', entry).pipe(
-        catchError(this.handleError),
-        map(() => entry)
-        )  
-      }
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+          )  
+      })
+    )
+  }
       
   delete(id: number): Observable<any> {
     const url = `${this.apiPath}/${id}`;
