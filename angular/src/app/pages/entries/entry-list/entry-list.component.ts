16bffe5f82ce27f4ad318450877e2ced788983b4
ericksonferreira <erickson.rinho@hotmail.com>
2020-03-19 20:55:02 -0300

    melhorando EntriesModule

--- angular/src/app/pages/entries/entry-list/entry-list.component.ts
+++ angular/src/app/pages/entries/entry-list/entry-list.component.ts
@@ -16,7 +16,7 @@ export class EntryListComponent implements OnInit {
 
   ngOnInit(): void {
     this.entryService.getAll().subscribe(
-      entries => this.entries = entries,
+      entries => this.entries = entries.sort((a,b) => b.id - a.id),
       error => alert('Error ao carregar a lista')
     )
   }
