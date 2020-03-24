ba563669bfa949604f38951b34219aded6934e17
ericksonferreira <erickson.rinho@hotmail.com>
2020-03-24 12:45:22 -0300

    adicionando BaseResourceModel

--- angular/src/app/pages/entries/shared/entry.model.ts
+++ angular/src/app/pages/entries/shared/entry.model.ts
@@ -1,22 +1,25 @@
+import { BaseResourceModel } from '../../../shared/models/base-resource.model';
 import { Category } from '../../categories/shared/category.model';
 
-export class Entry {
+export class Entry extends BaseResourceModel {
     constructor(
-        public id?:number,
-        public name?:string,
-        public description?:string,
-        public type?:string,
-        public amount?:string,
-        public date?:string,
-        public paid?:boolean,
-        public categoryId?:number,
-        public category?:Category
-    ){}
+        public id?: number,
+        public name?: string,
+        public description?: string,
+        public type?: string,
+        public amount?: string,
+        public date?: string,
+        public paid?: boolean,
+        public categoryId?: number,
+        public category?: Category
+    ) {
+      super();
+    }
 
     static types = {
         expense: 'Despesa',
         renevus: 'Receita'
-    }
+    };
     get paidText(): string {
         return this.paid ? 'Pago' : 'Pedente';
     }
