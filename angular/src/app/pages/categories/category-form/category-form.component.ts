71588bc2492930ca17ae79d63a92fe58efe04222
ericksonferreira <erickson.rinho@hotmail.com>
2020-03-26 15:35:02 -0300

    melhorando EntryService com BaseResourceService

--- angular/src/app/pages/categories/category-form/category-form.component.ts
+++ angular/src/app/pages/categories/category-form/category-form.component.ts
@@ -37,41 +37,41 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked  {
     this.loadCategory();
   }
 
-  ngAfterContentChecked(){
+  ngAfterContentChecked() {
     this.setPageTitle();
   }
 
   submitForm(){
     this.submittingForm = true;
-    if (this.currentAction == 'new'){
+    if (this.currentAction == 'new') {
       this.createCategory();
-    }else{
+    } else {
       this.updateCategory();
     }
   }
 
   // Métodos privados
 
-  //Verifica se é a pagina de editar ou de nova categoria
+  // Verifica se é a pagina de editar ou de nova categoria
   private setCurrentAction(){
     if(this.route.snapshot.url[0].path == 'new'){
       this.currentAction = 'new';
-    }else{
+    } else {
       this.currentAction = 'edit';
     }
   }
 
-  //Deixa os campos do formulário vazios
-  private buildCategoryForm(){
+  // Deixa os campos do formulário vazios
+  private buildCategoryForm() {
     this.categoryForm = this.formBuilder.group({
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
       description: [null]
-    })
+    });
   }
 
-  //Caso o formulário seja de edição, preenche os dados do mesmo com os dados da categoria
-  private loadCategory(){
+  // Caso o formulário seja de edição, preenche os dados do mesmo com os dados da categoria
+  private loadCategory() {
     if(this.currentAction == 'edit') {
       this.route.paramMap.pipe(
         switchMap(params => this.categoryService.getById(+params.get('id')))
@@ -82,7 +82,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked  {
           this.categoryForm.patchValue(category); // O patchValue preeche o formulario de Editar Formulário
         },
         (error) => alert('Ocorreu um erro no servidor, tente mais tarde')
-      )
+      );
     }
   }
 
@@ -90,8 +90,8 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked  {
   private setPageTitle(){
     if(this.currentAction == 'new') {
       this.pageTitle = 'Cadastro de Nova Categoria';
-    }else{
-      const categoryName = this.category.name || ';
+    } else {
+      const categoryName = this.category.name || '';
       this.pageTitle = 'Editando Categoria: ' + categoryName;
     }
   }
@@ -119,7 +119,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked  {
     toastr.success('Solicitação processada com sucesso!');
 
     // redireciona e recarrega a pagina do component
-    this.router.navigateByUrl('categories', {skipLocationChange:true}).then(
+    this.router.navigateByUrl('categories', {skipLocationChange: true}).then(
       () => this.router.navigate(['categories', category.id, 'edit'])
     )
   }
@@ -132,7 +132,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked  {
     if( error.status == 422 ) {
       this.serverErrorMessages = JSON.parse(error._body).errors;
       // Vai retornar neste estilo: ['Nome já existe', 'Descrição está invalida']
-    }else{
+    } else {
       this.serverErrorMessages = ['Falha na comunicação com o sevidor. Por favor, tente mais tarde.']
     }
   }
--- angular/src/app/pages/entries/shared/entry.service.ts
+++ angular/src/app/pages/entries/shared/entry.service.ts
@@ -1,92 +1,52 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-import { Observable, throwError } from 'rxjs';
+import { Injectable, Injector } from '@angular/core';
+import { Observable } from 'rxjs';
 import { map, catchError, flatMap } from 'rxjs/operators';
 
+import { BaseResourceService } from '../../../shared/services/base-resource.service';
 import { CategoryService } from '../../categories/shared/category.service';
-
 import { Entry } from './entry.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EntryService {
+export class EntryService extends BaseResourceService<Entry> {
 
-  private apiPath = 'api/entries';
-  constructor( private http: HttpClient, private categoryService: CategoryService ) { }
-
-  getAll(): Observable<Entry[]> {
-    return this.http.get(this.apiPath).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToCategories)
-    );
-  }
-
-  getById(id: number): Observable<Entry> {
-    const url = `${this.apiPath}/${id}`;
-    return this.http.get(url).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
-    );
-  }
+  constructor( protected injector: Injector, private categoryService: CategoryService ) {
+    super('api/entries', injector);
+   }
 
   create(entry: Entry): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
         entry.category = category;
 
-        return this.http.post(this.apiPath, entry).pipe(
-          catchError(this.handleError),
-          map(this.jsonDataToEntry)
-          );
+        return super.create(entry);
       })
-    );
-  }
+      );
+    }
 
   update(entry: Entry): Observable<Entry> {
-    const url = `${this.apiPath}/${entry.id}`;
-
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
         entry.category = category;
 
-        return this.http.put(url, entry).pipe(
-          catchError(this.handleError),
-          map(() => entry)
-          );
+        return super.update(entry);
       })
     );
   }
 
-  delete(id: number): Observable<any> {
-    const url = `${this.apiPath}/${id}`;
-    return this.http.delete(url).pipe(
-      catchError(this.handleError),
-      map(() => null)
-    );
-  }
-
-
     // Métodos privados
-  private jsonDataToCategories(jsonData: any[]): Entry[] {
+  protected jsonDataToResources(jsonData: any[]): Entry[] {
     const entries: Entry[] = [];
 
     jsonData.forEach(el => {
         const entry =  Object.assign(new Entry(), el);
         entries.push(entry);
     });
-
     return entries;
   }
 
-  private jsonDataToEntry(jsonData: any): Entry {
+  protected jsonDataToResource(jsonData: any): Entry {
     return Object.assign(new Entry(), jsonData);
   }
-
-  private handleError(error: any): Observable<any> {
-    console.log('Erro na requisição -> ', error);
-    return throwError(error);
-  }
-
 }
