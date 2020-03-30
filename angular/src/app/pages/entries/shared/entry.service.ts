ad98106d7e12d0e8147c32db383ccf3a05edd3f5
ericksonferreira <erickson.rinho@hotmail.com>
2020-03-29 23:45:56 -0300

    melhorando EntryService

--- angular/src/app/pages/entries/shared/entry.service.ts
+++ angular/src/app/pages/entries/shared/entry.service.ts
@@ -16,22 +16,20 @@ export class EntryService extends BaseResourceService<Entry> {
    }
 
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-
-        return super.create(entry);
-      })
-      );
-    }
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
+  }
 
   update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+  }
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
         entry.category = category;
-
-        return super.update(entry);
-      })
+        return sendFn(entry)
+      }),
+      catchError(this.handleError)
     );
   }
 }
