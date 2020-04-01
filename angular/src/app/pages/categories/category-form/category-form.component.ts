cb7da0fdb07a2453cfe8a068ea1ec4bd4bd09757
ericksonferreira <erickson.rinho@hotmail.com>
2020-04-01 20:53:07 -0300

    refatorando EntryFormComponent

--- angular/src/app/pages/categories/category-form/category-form.component.ts
+++ angular/src/app/pages/categories/category-form/category-form.component.ts
@@ -35,7 +35,7 @@ export class CategoryFormComponent extends BaseResourceFormComponent<Category> {
   }
 
   protected editionPageTitle(): string {
-    const categoryName = this.resource.name || '';
-    return 'Editando Categoria: ' + categoryName;
+    const resourceName = this.resource.name || '';
+    return 'Editando Categoria: ' + resourceName;
   }
 }
