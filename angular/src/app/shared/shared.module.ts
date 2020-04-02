fd7b1b831b2ad5a9668e54df3dda24b016f923dc
ericksonferreira <erickson.rinho@hotmail.com>
2020-04-01 22:28:10 -0300

    adicionando RouterModulle no SharedModule e Melhorando as paginas do componente com BreadCrumbComponent

--- angular/src/app/shared/shared.module.ts
+++ angular/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BredCrumbComponent } from './components/bred-crumb/bred-crumb.component';
 
@@ -10,11 +11,13 @@ import { BredCrumbComponent } from './components/bred-crumb/bred-crumb.component
   declarations: [BredCrumbComponent],
   imports: [
     CommonModule,
+    RouterModule,
     ReactiveFormsModule
   ],
   exports: [
     // shared modules
     CommonModule,
+    RouterModule,
     ReactiveFormsModule,
     BredCrumbComponent
   ]
